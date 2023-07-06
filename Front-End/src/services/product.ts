22a42d2af76446c2b89efe7535bce7c5e5f0f57b
MinhDuc <ogdp.vn@gmail.com>
2023-07-07 00:24:21 +0700

    Trang sản phẩm + Lọc danh mục + Sắp xếp

--- Front-End/src/services/product.ts
+++ Front-End/src/services/product.ts
@@ -1,47 +1,55 @@
-import { IProduct } from "../types/product"
-import intansce from "./intansce"
+import { IProduct } from "../types/product";
+import intansce from "./intansce";
 export const GetAllProduct = () => {
-    return intansce.get('/products')
-}
+  return intansce.get("/products");
+};
 export const GetProductSale = () => {
-    return intansce.get('/products/sale')
-}
+  return intansce.get("/products/sale");
+};
 export const SearchProductByName = (name: string) => {
-    return intansce.get('/products/search?name=' + name)
-}
-export const FindProductByPrice = (min:number,max:number) => {
-    return intansce.get(`/products/filter/price?minPrice=${min}&maxPrice=${max}&sortType=desc`)
-}
+  return intansce.get("/products/search?name=" + name);
+};
+export const FindProductByPrice = (min: number, max: number) => {
+  return intansce.get(
+    `/products/filter/price?minPrice=${min}&maxPrice=${max}&sortType=desc`
+  );
+};
+export const ProductSort = (value: string) => {
+  if (value === null || value === undefined) {
+    return intansce.get("/products");
+  }
+  return intansce.get("/products/sortyes?sort_name=" + value);
+};
 export const FilterProductByCategory = (id: string) => {
-    if (id === null || id === undefined) {
-        return intansce.get('/products')
-    }
-    return intansce.get('/products/filter?CategoryId=' + id)
-}
+  if (id === null || id === undefined) {
+    return intansce.get("/products");
+  }
+  return intansce.get("/products/filter?CategoryId=" + id);
+};
 export const GetOneProduct = (_id: string) => {
-    return intansce.get('/products/' + _id)
-}
+  return intansce.get("/products/" + _id);
+};
 
 export const CreateProduct = (data: IProduct) => {
-    return intansce.post(`/products`, data, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-        },
-    });
-}
+  return intansce.post(`/products`, data, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
+  });
+};
 
 export const UpdateProduct = (data: IProduct) => {
-    return intansce.put(`/products/${data._id}`, data, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-        },
-    });
-}
+  return intansce.put(`/products/${data._id}`, data, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
+  });
+};
 
 export const RemoveProduct = async (_id: string) => {
-    return intansce.delete(`/products/${_id}`, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-        },
-    });
-}
+  return intansce.delete(`/products/${_id}`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
+  });
+};
